49c9e62e92eafbbf0a2ebdfbb23d0bf3a8c66471
deepurpwebapps <71063648+deepurpwebapps@users.noreply.github.com>
2020-11-04 15:24:48 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DashbaordComponent } from './dashbaord/dashbaord.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard', component:DashbaordComponent 
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: '', redirectTo: 'dashboard', pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashbaordComponent } from './dashbaord/dashbaord.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbaordComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
